959b5a8b6568d087f5b4b22982141396d553a43d
beigenfeld <beigenfeld@gmail.com>
2017-11-03 13:19:20 -0500

    Commit to pull

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,9 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Server
+namespace Server   
 {
-    class Server
+    class Server : IDictionary;
     {
         public static Client client;
         TcpListener server;
