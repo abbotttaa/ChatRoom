8f5b23e098370edc68e3f570f363eb593c39a016
Taylor Abbott <abbotttaa@gmail.com>
2017-11-05 16:23:50 -0600

    dictonary and queue improved. Basic func. still not reached.

--- Server/Client.cs
+++ Server/Client.cs
@@ -11,13 +11,12 @@ namespace Server
     {
         NetworkStream stream;
         TcpClient client;
-        public string UserId;
+        public int UserId;
         public Client(NetworkStream Stream, TcpClient Client)
         {
             stream = Stream;
             client = Client;
-            Console.WriteLine();
-            UserId = "495933b6-1762-47a1-b655-483510072e73";
+            UserId = 0;
         }
         public void Send(string Message)
         {
@@ -30,12 +29,11 @@ namespace Server
         }
         public string Recieve()
         {
-                byte[] recievedMessage = new byte[256];
-                stream.Read(recievedMessage, 0, recievedMessage.Length);
-                string recievedMessageString = Encoding.ASCII.GetString(recievedMessage);
-                Console.WriteLine(recievedMessageString);
-                
-                return recievedMessageString;
+            byte[] recievedMessage = new byte[256];
+            stream.Read(recievedMessage, 0, recievedMessage.Length);
+            string recievedMessageString = Encoding.ASCII.GetString(recievedMessage);
+            Console.WriteLine(recievedMessageString);
+            return recievedMessageString;
 
         }
 
